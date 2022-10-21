54b3903b9e750e7d77662dc964b4e3a6e3f4caa4
Marshall Jones <mjone032@wch135-01.cs.ucr.edu>
2022-10-21 16:35:06 -0700

    initial

---
+++ lab4_input.sql
@@ -0,0 +1,11 @@
+insert into Books values (0001, 'Your Mom lmao', 'Your Dad lmao', 1969);
+insert into Books values (0002, 'Your mama lol', 'Your papa lol', 6919);
+
+insert into Students values(1000, 'Marshall', 'CS', 22);
+insert into Students values(1001, 'Nicole', 'EE', 21);
+
+insert into Authors values('Eugene Krabs', 'Bikini Bottom');
+insert into Authors values('Plankton', 'Chum Bucket');
+
+
+
